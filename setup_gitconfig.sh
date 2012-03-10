0f1c7c8624c02a18f2af78bd74ff856b862018c9
Bradley Grzesiak <brad@bendyworks.com>
2012-03-10 11:35:29 -0600

    Use script to setup .gitconfig

---
+++ setup_gitconfig.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+git config --global color.ui auto
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.me merge
+git config --global alias.br branch
